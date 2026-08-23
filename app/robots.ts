878445067df1e5e1c645276bc8d735f9c422fbad
formstudioweb <formstudio956@gmail.com>
2026-08-23 17:01:06 +0200

    Delete app directory

--- app/robots.ts
+++
@@ -1,5 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://obscura-tattoo.melnichenkomariia.chatgpt.site/sitemap.xml", host: "https://obscura-tattoo.melnichenkomariia.chatgpt.site" };
-}
--- app/sitemap.ts
+++
@@ -1,5 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [{ url: "https://obscura-tattoo.melnichenkomariia.chatgpt.site", lastModified: new Date(), changeFrequency: "monthly", priority: 1 }];
-}
